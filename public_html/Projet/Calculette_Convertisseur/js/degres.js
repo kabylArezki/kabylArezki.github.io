3e67dd388ce77ae161fe2388d3d1d657af2a4ca9
kerkara <Arezki.Kerkar@e.ujf-grenoble.fr>
2016-10-12 22:13:28 +0200

    Convertisseur de températeures

---
+++ public_html/Projet/Calculette_Convertisseur/js/degres.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function convertirTvsV() {
+    
+var t = parseInt(document.getElementById("op1").value);
+var f=((9/5)*t)+32;
+
+  
+    document.getElementById("op2").value = f;
+
+}
+function convertirFvsT(){
+        var f = parseInt(document.getElementById("op2").value);
+        var t=(5/9)*(f-32);
+        
+          document.getElementById("op1").value = t;
+}
