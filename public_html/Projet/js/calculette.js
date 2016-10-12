ad0dd875cb5ea89b3be89b39a74106fccd39eb80
kerkara <Arezki.Kerkar@e.ujf-grenoble.fr>
2016-10-12 21:37:27 +0200

    callculette

---
+++ public_html/Projet/js/calculette.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function calculer() {
+    var operande1 = parseInt(document.getElementById("op1").value);
+    var operande2 = parseInt(document.getElementById("op2").value);
+    var operateur = document.getElementById("operateur").value;
+
+  
+    var res;
+
+    switch (operateur) {
+        case "+" :
+            res = operande1 + operande2;
+            break;
+        case "-" :
+            res = operande1 - operande2;
+            break;
+        case "*" :
+            res = operande1 * operande2;
+            break;
+        case "/" :
+            res = operande1 / operande2;
+            break;
+    }
+    document.getElementById("resultat").innerHTML = res;
+
+}
+
+
+
