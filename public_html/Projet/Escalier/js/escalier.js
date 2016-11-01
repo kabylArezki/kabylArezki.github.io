bd2306e93fd2f17bf05f0b0195def52775754033
kerkara <Arezki.Kerkar@e.ujf-grenoble.fr>
2016-11-01 18:10:39 +0100

    Escalier

--- public_html/Projet/Escalier/js/escalier.js
+++ public_html/Projet/Escalier/js/escalier.js
@@ -24,7 +24,7 @@ function effacer() {
     var nbr = parseFloat(document.getElementById("nbr").value);
     var ctx = document.getElementById("c").getContext("2d");
        for (var i = 0; i < nbr; i++) {
-        ctx.clearRect(0,h*i,h*(i+1),h);
+        ctx.clearRect(0,0,800,800);
     }
     
 }
