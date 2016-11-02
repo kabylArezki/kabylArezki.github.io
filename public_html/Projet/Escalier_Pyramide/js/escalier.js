445ffcc1beade70333f1dbc2d936ade118e3432b
kerkara <Arezki.Kerkar@e.ujf-grenoble.fr>
2016-11-02 21:55:49 +0100

    added Escalier_Pyramide

---
+++ public_html/Projet/Escalier_Pyramide/js/escalier.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+function dessiner() {
+    var h = parseFloat(document.getElementById("H").value);
+    var nbr = parseFloat(document.getElementById("nbr").value);
+    var ctx = document.getElementById("c").getContext("2d");
+    for (var i = 0; i < nbr; i++) {
+        ctx.fillStyle="red";
+        ctx.fillRect(0,h*i,h*(i+1),h);
+        ctx.strokeStyle="#ffffff";
+        ctx.strokeRect(0,h*i,h*(i+1),h);
+    }
+}
+
+function effacer() {
+        var h = parseFloat(document.getElementById("H").value);
+    var nbr = parseFloat(document.getElementById("nbr").value);
+    var ctx = document.getElementById("c").getContext("2d");
+       for (var i = 0; i < nbr; i++) {
+        ctx.clearRect(0,0,800,800);
+    }
+    
+}
